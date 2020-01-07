a503536fe85342b4392bd05edc279464e54eb3f7
Ikem Nomso <ikemnomso@gmail.com>
2020-01-07 12:07:30 +0100

    Updates

---
+++ precache-manifest.4028a6f0529bac69b8839064d5e34c21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e4e0ec20c4a3976afecb568fe547548",
+    "url": "/avatar-finder/index.html"
+  },
+  {
+    "revision": "71ffe58ac388b273571a",
+    "url": "/avatar-finder/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b9aac8ebb8aabc9d7859",
+    "url": "/avatar-finder/static/css/main.c936c3cd.chunk.css"
+  },
+  {
+    "revision": "71ffe58ac388b273571a",
+    "url": "/avatar-finder/static/js/2.55a6a666.chunk.js"
+  },
+  {
+    "revision": "b9aac8ebb8aabc9d7859",
+    "url": "/avatar-finder/static/js/main.25e5dddd.chunk.js"
+  },
+  {
+    "revision": "677c9105e836266230f7",
+    "url": "/avatar-finder/static/js/runtime-main.8e433fff.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/avatar-finder/static/media/SEGA.12a3db8b.woff"
+  }
+]);
